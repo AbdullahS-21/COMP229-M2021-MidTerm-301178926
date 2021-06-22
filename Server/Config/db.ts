5009af3f5a4b650ab177ef933ce5b55e95ccf611
AbdullahS-21 <abdullahshaikh225@gmail.com>
2021-06-22 15:57:32 +0530

    final commit

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
-export const HostName = "LocalHost";
+export const RemoteURI = "mongodb+srv://admin:A123456@cluster-comp229-a2.sr1ho.mongodb.net/COMP229-M2021-MidTerm-301178926?retryWrites=true&w=majority";
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
--- Server/Routes/books.js
+++ Server/Routes/books.js
@@ -22,13 +22,69 @@ router.get('/', (req, res, next) => {
     });
 });
 router.get('/add', (req, res, next) => {
+    res.render('books/details', {
+        title: 'Books',
+        page: 'books',
+        books: ''
+    });
 });
 router.post('/add', (req, res, next) => {
+    console.log(req.body);
+    let newBook = new books_1.default({
+        "Title": req.body.title,
+        "Description": "",
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    });
+    books_1.default.create(newBook, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books');
+    });
 });
 router.get('/:id', (req, res, next) => {
+    let id = req.params['id'];
+    books_1.default.findById(id, {}, {}, (err, item) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('books/details', {
+            title: 'Books',
+            page: 'books',
+            books: item
+        });
+    });
 });
 router.post('/:id', (req, res, next) => {
+    let id = req.params.id;
+    let updateBook = new books_1.default({
+        "_id": id,
+        "Title": req.body.title,
+        "Description": "",
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    });
+    books_1.default.updateOne({ _id: id }, updateBook, {}, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books');
+    });
 });
 router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+    books_1.default.remove({ _id: id }, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books');
+    });
 });
 //# sourceMappingURL=books.js.map
